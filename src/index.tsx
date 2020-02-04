e507168ab01e306adcd6668837eafd13a63114ec
bharding2 <b.harding@gmail.com>
2020-02-04 15:21:36 -0800

    initialize react app

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App: React.FunctionComponent = () => {
+  return <div>Slothbear Builder</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
